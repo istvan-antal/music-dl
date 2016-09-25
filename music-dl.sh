9be77e44b1b738726588f2c10038da1f45d6bc69
István Antal <istvan.m.antal@gmail.com>
2016-09-25 13:14:06 +0200

    Remove temporary files

--- music-dl.sh
+++ music-dl.sh
@@ -6,7 +6,9 @@ youtube-dl -x --write-description --audio-format mp3 "https://www.youtube.com/wa
 wget -O "$VIDEO_ID.jpg"  "http://img.youtube.com/vi/$VIDEO_ID/maxresdefault.jpg"
 MP3_FILE=$(find . -iname "*.mp3" -print)
 DESCRIPTION_FILE=$(find . -iname "*.description" -print)
-#mv "$MP3_FILE" "$MP3_FILE-source"
-#ffmpeg -i "$MP3_FILE-source" -i "$VIDEO_ID.jpg" -map_metadata 0 -map 0 -map 1 "$MP3_FILE"
 eyeD3 --add-image="$VIDEO_ID.jpg:OTHER" --add-lyrics="$DESCRIPTION_FILE" "$MP3_FILE"
+rm "$DESCRIPTION_FILE"
+rm "$VIDEO_ID.jpg"
+mv "$MP3_FILE" ../
 cd ..
+rm -r "$VIDEO_ID"
