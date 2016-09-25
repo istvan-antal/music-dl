595cc3e61480060aa2d74352f79d8029bde02333
István Antal <istvan.m.antal@gmail.com>
2016-09-25 13:31:43 +0200

    Use uploader name as artist

--- music-dl.sh
+++ music-dl.sh
@@ -2,13 +2,15 @@
 VIDEO_ID=$1
 mkdir $VIDEO_ID
 cd $VIDEO_ID
-youtube-dl -x --write-description --audio-format mp3 "https://www.youtube.com/watch?v=$VIDEO_ID"
-wget -O "$VIDEO_ID.jpg"  "http://img.youtube.com/vi/$VIDEO_ID/maxresdefault.jpg"
+youtube-dl -x --add-metadata --embed-thumbnail --write-info-json --write-description --audio-format mp3 "https://www.youtube.com/watch?v=$VIDEO_ID"
+#wget -O "$VIDEO_ID.jpg"  "http://img.youtube.com/vi/$VIDEO_ID/maxresdefault.jpg"
 MP3_FILE=$(find . -iname "*.mp3" -print)
 DESCRIPTION_FILE=$(find . -iname "*.description" -print)
-eyeD3 --add-image="$VIDEO_ID.jpg:OTHER" --add-lyrics="$DESCRIPTION_FILE" "$MP3_FILE"
+JSON_FILE=$(find . -iname "*.json" -print)
+UPLOADER=$(cat "$JSON_FILE" |  python -c "import json,sys;obj=json.load(sys.stdin);print obj['uploader'];")
+eyeD3 --artist="$UPLOADER" --add-lyrics="$DESCRIPTION_FILE" "$MP3_FILE"
 rm "$DESCRIPTION_FILE"
-rm "$VIDEO_ID.jpg"
+rm "$JSON_FILE"
 mv "$MP3_FILE" ../
 cd ..
 rm -r "$VIDEO_ID"
