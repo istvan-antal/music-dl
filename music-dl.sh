f73a92f3e408aecc7ff9f58c47dc0a6876c60554
István Antal <istvan.m.antal@gmail.com>
2016-09-25 13:31:59 +0200

    Remove unsued code

--- music-dl.sh
+++ music-dl.sh
@@ -3,7 +3,6 @@ VIDEO_ID=$1
 mkdir $VIDEO_ID
 cd $VIDEO_ID
 youtube-dl -x --add-metadata --embed-thumbnail --write-info-json --write-description --audio-format mp3 "https://www.youtube.com/watch?v=$VIDEO_ID"
-#wget -O "$VIDEO_ID.jpg"  "http://img.youtube.com/vi/$VIDEO_ID/maxresdefault.jpg"
 MP3_FILE=$(find . -iname "*.mp3" -print)
 DESCRIPTION_FILE=$(find . -iname "*.description" -print)
 JSON_FILE=$(find . -iname "*.json" -print)
